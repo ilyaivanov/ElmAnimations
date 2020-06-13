8a8fc6cdddd3c8ef8f0a35158ffc5583e85a9ee5
Ilya <static.ila@gmail.com>
2020-06-13 15:37:32 +0300

    Add a static tree layout with hardcoded nodes

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,71 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h3, text)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
+import Tree exposing (Children, TreeItem, getResponses, initialNodes)
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 
-
--- MODEL
+type alias Flags =
+    {}
 
 
 type alias Model =
-    Int
+    { nodes : List TreeItem
+    }
 
 
-init : Model
-init =
-    0
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { nodes = initialNodes }
+    , Cmd.none
+    )
 
 
-
--- UPDATE
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
 type Msg
-    = Increment
-    | Decrement
+    = None
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
+        None ->
+            ( model, Cmd.none )
 
 
-
--- VIEW
+type alias TreeItemUiModel =
+    { title : String
+    , level : Int
+    }
 
 
 view : Model -> Html Msg
 view model =
+    let
+        getChild : Int -> TreeItem -> List TreeItemUiModel
+        getChild level n =
+            case n.children of
+                Just children ->
+                    List.append [ { title = n.title, level = level } ] (List.concat (List.map (getChild (level + 1)) (getResponses children)))
+
+                Nothing ->
+                    [ { title = n.title, level = level } ]
+
+        flatNodes =
+            List.concat (List.map (getChild 0) model.nodes)
+    in
     div []
-        [ h3 [ class "bar" ] [ text "My Title" ]
-        , button [ onClick Decrement, class "foo" ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+        (List.map viewNode flatNodes)
+
+
+viewNode node =
+    div [ style "margin-left" (String.fromInt (node.level * 20) ++ "px") ] [ text node.title ]
